5dae4e31aa66700d575810f718630912799862c7
Nazar <nazarchampion@gmail.com>
2018-06-05 22:19:44 +0600

    some change

--- app.js
+++ app.js
@@ -1,52 +1,87 @@
 
+import fetch from "node-fetch";
+import { prompt } from "inquirer";               
 
-const fetch = require('node-fetch');
-const inquirer = require('inquirer');
+prompt([
+  {
+    type: 'input',
+    name: 'teamname',
+    message: 'Your Team\'s name'
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Your  Username'
+  },
+  {
+    type: 'input',
+    name: 'reponame',
+    message: 'Your Repository name'
+  }
+]).then( answers => {
+  
+  fetch(`https://api.bitbucket.org/2.0/repositories/${answers.teamname}/`) //name of the inserted team 
+    .then(res => res.json())
+    .then(json => {
+      json.values.filter(vals => {
 
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'username',
-        message: 'Your Username'
-      },
-      {
-        type: 'input',
-        name: 'reponame',
-        message: 'Your Repository name'
-      }
-    ]).then((answers) => {
-      //console.log('Hello ' + answers.username);
-      fetch(`https://api.bitbucket.org/2.0/teams/${answers.username}/`)
-          .then(res => res.json())
-          .then(json => {
-            //repository api link
-            const hrefs = json.links.repositories.href;
+        //api links of individual repository
+        const repositoryApiLinks = vals.links.pullrequests.href;
 
-            //console.log(hrefs);
-            fetch(hrefs)
-              .then(res => res.json())
-              .then(json => {
-                json.values.filter(req => {
+        //validation of repository name
+        if(answers.reponame === vals.name || 
+            answers.reponame === vals.slug) {
 
-                  //pull request api links of each repository
-                  const pullreqs = req.links.pullrequests.href;
+              fetch(repositoryApiLinks)
+                .then(res => res.json())
 
-                  //if typed repository name equals to existing repo name
-                  if (answers.reponame === req.name) {
-                    fetch(pullreqs)
+                //by default the length of a page = 10, 
+                //so we travercing the next pages to get 
+                //the rest values, until we hit the last page, 
+                //so we use function recursively, to go as deep as possible    
+                .then(function getNextPage(json) {
+                  
+                  if (json.next) {
+                    fetch(json.next)
                       .then(res => res.json())
                       .then(json => {
-                        json.values.map( prs => {
-                          const prsFromTeamMatesThatShouldBeReviewed = prs.links.html.href;
-                          //status should be OPEN
-                          if (prs.state === "OPEN") {
-                            console.log(prsFromTeamMatesThatShouldBeReviewed);
-                          }
-                        });// MAP method
-                      })// inner JSON
-                  }// IF end
-                })// FILTER method END
-              });// middle outer JSON
+                        return getNextPage(json)
+                      })
+                  }
+                  json.values.filter(repo => {
+                    //PR's status should be "OPEN"
+                    if(repo.state === "OPEN") {
+                      const activity = repo.links.activity.href;
+                      
+                      fetch(activity)
+                        .then(res => res.json())
+                        .then(json => {
 
-          });//outer JSON
-    });
+                          //each element of the 'values' 
+                          //array has PR links, it doesnt matter which element 
+                          //take, and we took the first element
+                          const pullRequestApiLink = json.values[0].pull_request.links.self.href;
+
+                          fetch(pullRequestApiLink)
+                            .then(res => res.json())
+                            .then(json => {
+                              //get only PR links related to the typed username
+                              //in other words - filter reviewers by username
+                              json.reviewers.filter(rev => {
+                                if (answers.username === rev.username) {
+                                  const prsFromTeammatesThatShouldBeReviewed = json.links.html.href;
+                                  console.log(prsFromTeammatesThatShouldBeReviewed);
+                                } //IF ends
+                              })
+                            })
+                        })
+                      
+                    }
+                  })                 
+                  
+                });
+            } //IF outer end
+      }); // FILTER outer
+      
+    })
+}); 
